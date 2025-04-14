7f7db201ff492d458cbb1ecdea8494dde7209ea4
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-04-15 09:45:07 +1000

    retry generation of project.pbxproj
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/bin/prebuildConfig.sh
+++ app/bin/prebuildConfig.sh
@@ -67,3 +67,8 @@ if test -f /usr/libexec/PlistBuddy; then
   /usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:0:CFBundleURLSchemes:0 $VITE_APP_ID" ./ios/App/App/Info.plist
   /usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:0:CFBundleURLName $VITE_APP_ID" ./ios/App/App/Info.plist
 fi
+
+
+# update project file for local build
+sed -e "s/${APP_ID_PLACEHOLDER}/${APPLE_BUNDLE_IDENTIFIER}/g" ./ios/App/App.xcodeproj/project.pbxproj.dist |\
+  sed -e "s/APP_STORE_CONNECT_TEAM_ID/${APP_STORE_CONNECT_TEAM_ID}/g" > ./ios/App/App.xcodeproj/project.pbxproj
